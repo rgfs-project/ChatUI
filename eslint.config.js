d2990d775e662c20eee2233bddb506901869dbb3
user <322349353+@users.noreply.github.com>
2026-09-17 08:32:45 -0700

    ignore inspect.mjs in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'coverage', 'node_modules', 'data'] },
+  { ignores: ['dist', 'coverage', 'node_modules', 'data', 'inspect.mjs'] },
 
   js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
